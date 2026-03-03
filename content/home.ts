83a970d18b7d0ccbe1e1cdd99857a67c336cf6f3
Tolnay Barni <barnabas@tolnay.eu>
2026-03-03 19:32:33 +0100

    Revert "Version 1.0 of LeafmcWeb"
    This reverts commit 35bfd6558fbf0ea17db80705df2ca29861493966.

--- content/home.ts
+++
@@ -1,3 +0,0 @@
-// Content is now loaded at request time from config/home.json (+ locale overlays).
-// Import types from here; import getHomeConfig from @/lib/config to read values.
-export type { HomeConfig } from '@/lib/config'
--- content/nav.ts
+++
@@ -1,3 +0,0 @@
-// Content is now loaded from config/nav.json.
-// Import types from here; import getNavConfig from @/lib/config to read values.
-export type { NavConfig } from '@/lib/config'
